384eadf43a735e3e67a6c1c2a0e48097c5a52cbb
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-06-26 23:56:11 -0300

    Colocando o gulp autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./index.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 100 versions']
+    }))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./index.scss', ['sass']);
 });
+
