0bcdb9b1f096673b39b6d51ef992b79e541c9678
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-06-27 22:18:56 -0300

    Criando 'arquivos finais'

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const htmlclean = require('gulp-htmlclean');
+const rename = require("gulp-rename");
 
 gulp.task('sass', function () {
   return gulp.src('./index.scss')
@@ -11,7 +13,14 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./index.scss', ['sass']);
+gulp.task('htmlclean', function () {
+  return gulp.src('./index.src.html')
+    .pipe(htmlclean())
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('.'));
 });
 
+gulp.task('watch', function () {
+  gulp.watch('./index.scss', ['sass']);
+  gulp.watch('./index.src.html', ['htmlclean']);
+});
