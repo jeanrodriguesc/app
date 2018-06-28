df5eb47bffe07e243b3126bee68274cbee6c0ff1
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-06-27 22:54:36 -0300

    Organizando pasta final

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,36 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const htmlclean = require('gulp-htmlclean');
-const rename = require("gulp-rename");
+
+const final = './final';
 
 gulp.task('sass', function () {
-  return gulp.src('./index.scss')
+  return gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 100 versions']
     }))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest(final));
 });
 
-gulp.task('htmlclean', function () {
-  return gulp.src('./index.src.html')
+gulp.task('html', function () {
+  return gulp.src('./src/*.html')
     .pipe(htmlclean())
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest(final));
+});
+
+gulp.task('js', function () {
+  return gulp.src('./src/*.js')
+    .pipe(gulp.dest(final));
+});
+
+gulp.task('images', function () {
+  return gulp.src('./src/*.png')
+    .pipe(gulp.dest(final));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./index.scss', ['sass']);
-  gulp.watch('./index.src.html', ['htmlclean']);
+  gulp.watch('./src/*.scss', ['sass']);
+  gulp.watch('./src/*.html', ['html']);
+  gulp.watch('./src/*.js', ['js']);
 });
