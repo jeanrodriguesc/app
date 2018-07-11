107d805eea873b28ed9f7645ac2b85079e70a80e
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-07-10 22:54:25 -0300

    Tentando usar SweetAlert.2

--- testeSweetAlert.js
+++ testeSweetAlert.js
@@ -1,1 +1,10 @@
+$("#submit").click(function () {
+  var name = $('#name').val();
+  var email = $('#email').val();
+  var phone = $('#phone').val();
 
+  if (name == '' || email == '' || phone == '') {
+
+    alert('OI');
+  }
+})
