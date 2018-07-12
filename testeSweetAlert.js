288e02dd3cc0178a9cbd94bcff3ef48db623cea6
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-07-12 16:50:13 -0300

    Usando o teste.2

--- testeSweetAlert.js
+++ testeSweetAlert.js
@@ -1,6 +1,34 @@
 function testando() {
   var codigoProduto = document.getElementById("codigoProduto").value;
+  var nomeProduto = document.getElementById("nomeProduto").value;
+  var precoCompra = document.getElementById("precoCompra").value;
+  var precoVenda = document.getElementById("precoVenda").value;
+  var quantidade = document.getElementById("quantidade").value;
+  var fornecedor = document.getElementById("fornecedor").value;
 
-  if (codigoProduto.length > 2)
-    swal("Hello world!",  codigoProduto);
+
+  if (codigoProduto.length < 0 || codigoProduto.length > 30)
+    alert('Codigo invalido');
+  else {
+    if (nomeProduto.length < 0 || nomeProduto.length > 50)
+      alert('Nome do produto invalido');
+    else {
+      if (precoCompra <= 0)
+        alert('Preço de compra invalido');
+      else {
+        if (precoVenda <= 0)
+          alert('Preço de venda invalido');
+        else {
+          if (quantidade <= 0)
+            alert('Quantidade desconhecida');
+          else {
+            if (fornecedor.length < 0)
+              alert('Sem Fornecedor');
+            else
+              swal('Cadastro realizado com sucesso', codigoProduto + '\nNome do Produto: ' + nomeProduto + '\nPreço de compra: ' + precoCompra + '\nPreço de Venda: ' + precoVenda + '\nQuantidade: ' + quantidade + '\nFornecedor: ' + fornecedor);
+          }
+        }
+      }
+    }
+  }
 }
