0d55d5b7d42d4f62bdfd4038e05fb8480a03bfa7
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-07-12 17:38:42 -0300

    Colocando o teste.4 no cadastroProduto

--- src/index.js
+++ src/index.js
@@ -182,26 +182,26 @@ function enviarDados3() {
   var quantidade = document.getElementById("quantidade").value;
   var fornecedor = document.getElementById("fornecedor").value;
 
-  if (codigoProduto.length < 0 || codigoProduto.length > 30)
-    alert('Codigo invalido');
+
+  if (codigoProduto.length < 3 || codigoProduto.length > 30)
+    swal('Erro', 'Codigo invalido', 'error');
   else {
     if (nomeProduto.length < 0 || nomeProduto.length > 50)
-      alert('Nome do produto invalido');
+      swal('Erro', 'Nome invalido', 'error');
     else {
       if (precoCompra <= 0)
-        alert('Preço de compra invalido');
+        swal('Erro', 'Valor do preço de compra invalido', 'error');
       else {
         if (precoVenda <= 0)
-          alert('Preço de venda invalido');
+          swal('Erro', 'Valor do ´preço de venda invalido', 'error');
         else {
           if (quantidade <= 0)
-            alert('Quantidade desconhecida');
+            swal('Erro', 'Quantidade invalida', 'error');
           else {
-            if (fornecedor.length < 0)
-              alert('Sem Fornecedor');
+            if (fornecedor.length = 0)
+              swal('Erro', 'Fornecedor desconhecido', 'error');
             else
-              alert('Cadastro realizado com sucesso\nCodigo: ' + codigoProduto + '\nNome do Produto: ' + nomeProduto + '\nPreço de compra: ' + precoCompra + '\nPreço de Venda: ' + precoVenda + '\nQuantidade: ' + quantidade + '\nFornecedor: ' + fornecedor);
-
+              swal("Cadastro realizado com sucesso", '\nCodigo do Produto: ' + codigoProduto + '\nNome do Produto: ' + nomeProduto + '\nPreço de compra: ' + precoCompra + '\nPreço de Venda: ' + precoVenda + '\nQuantidade: ' + quantidade + '\nFornecedor: ' + fornecedor, "success");
           }
         }
       }
