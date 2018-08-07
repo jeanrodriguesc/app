9b02404590e2ff55a015f5978710942407120ee2
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-08-07 17:20:52 -0300

    Table teste.5 (js)

---
+++ table.js
@@ -0,0 +1,11 @@
+var dados = [
+  ["Jean Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["Lívia Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["Thiago Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["John Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["Jade Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["Jonas Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["Joel Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["Adriano Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"]
+];
+
