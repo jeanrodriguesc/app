5974a5eedddee9600927c83ce070f3da951856db
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-08-07 18:00:38 -0300

    Table teste.7] (js)

--- table.js
+++ table.js
@@ -19,3 +19,30 @@ for (var i = 0; i <dados.length; i++) {
     "</tr>";
   tbody.innerHTML += tr;
 }
+
+var tr = tbody.childNodes;
+
+
+document.getElementsById("txtBusca").addEventListener("keyup", function () {
+  var busca = document.getElementsById("txtBusca").value.toLowerCase();
+
+  for  (var i = 0; i < tbody.childNodes.length; i++) {
+   var achou = false;
+   var tr = tbody.childNodes[i];
+   var td = tr.childNodes;
+
+   for (var j =0; j <td.length; j++) {
+     var value = td[j].childNodes[0].nodeValue.toLowerCase();
+
+     if (value.indexOf(busca) >=0) {
+       achou = true
+     }
+   }
+if (achou) {
+  tr.style.display = "table-row";
+} else {
+     tr.style.display = "none"
+}
+
+  }
+});
