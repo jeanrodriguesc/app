7e9ca964edf6a77ae6f13c11632d3a3a8afc3031
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-08-09 23:56:58 -0300

    Table teste.10 (js & css)

--- table.js
+++ table.js
@@ -1,3 +1,5 @@
+var tbody = document.getElementById("tbody");
+
 var dados = [
   ["Jean Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
   ["Lívia Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Bloqueado"],
@@ -9,36 +11,36 @@ var dados = [
   ["Adriano Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"]
 ];
 
-var tbody = document.getElementById("tbody");
-for (var i = 0; i <dados.length; i++) {
+for (var i = 0; i < dados.length; i++) {
   var tr = "<tr>" +
-  "<td>" + dados [i][0] + "</td>" +
-  "<td>" + dados [i][1] + "</td>" +
-  "<td>" + dados [i][2] + "</td>" +
-  "<td>" + dados [i][3] + "</td>" +
+    "<td>" + dados [i][0] + "</td>" +
+    "<td>" + dados [i][1] + "</td>" +
+    "<td>" + dados [i][2] + "</td>" +
+    "<td>" + dados [i][3] + "</td>" +
     "</tr>";
   tbody.innerHTML += tr;
 }
 
 document.getElementById("txtBusca").addEventListener("keyup", function () {
+
   var busca = document.getElementById("txtBusca").value.toLowerCase();
 
-  for  (var i = 0; i < tbody.childNodes.length; i++) {
-   var achou = false;
-   var tr = tbody.childNodes[i];
-   var td = tr.childNodes;
+  for (var i = 0; i < tbody.childNodes.length; i++) {
+    var achou = false;
+    var tr = tbody.childNodes[i];
+    var td = tr.childNodes;
 
-   for (var j =0; j <td.length; j++) {
-     var value = td[j].childNodes[0].nodeValue.toLowerCase();
+    for (var j = 0; j < td.length; j++) {
+      var value = td[j].childNodes[0].nodeValue.toLowerCase();
 
-     if (value.indexOf(busca) >=0) {
-       achou = true
-     }
-   }
-if (achou) {
-  tr.style.display = "table-row";
-} else {
-     tr.style.display = "none"
-}
+      if (value.indexOf(busca) >= 0) {
+        achou = true
+      }
+    }
+    if (achou) {
+      tr.style.display = "table-row";
+    } else {
+      tr.style.display = "none"
+    }
   }
 });
