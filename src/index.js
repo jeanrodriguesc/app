f81de8e99d61068a3d088adf424f858b98805032
Jean Rodrigues <jeanrodrigues002@gmail.com>
2018-08-20 18:00:05 -0300

    tentando acertar

--- src/index.js
+++ src/index.js
@@ -1,37 +1,4 @@
-var productList = [];
 
-fetch('/produtos').then(function (response) {
-  return response.json()
-})
-  .then(function (products) {
-    productList.length = 0;
-    products.forEach(function (product) {
-      productList.push(product)
-    });
-    loadProductList()
-  });
-
-function onSearchChange(element) {
-  var search = element.value.toLowerCase();
-  var list = document.querySelector('.list');
-  list.innerHTML = '';
-  productList.forEach(function (product) {
-    if (product.toLowerCase().startsWith(search)) {
-      var item = document.createElement('li');
-      item.innerText = product;
-      list.appendChild(item)
-    }
-  })
-}
-
-function loadProductList() {
-  var list = document.querySelector('.list');
-  productList.forEach(function (product) {
-    var item = document.createElement('li');
-    item.innerText = product;
-    list.appendChild(item)
-  })
-}
 
 var historic = [
   'Arroz',
@@ -187,6 +154,57 @@ function filtro() {
   for (var i = 0; i < tbody.childNodes.length; i++) {
     var achou = false;
     var tr = tbody.childNodes[i];
+    var td = tr.childNodes;
+
+    for (var j = 0; j < td.length; j++) {
+      var value = td[j].childNodes[0].nodeValue.toLowerCase();
+
+      if (value.indexOf(busca) >= 0) {
+        achou = true
+      }
+    }
+    if (achou) {
+      tr.style.display = "table-row";
+    } else {
+      tr.style.display = "none"
+    }
+  }
+}
+
+
+
+
+
+var ttbody = document.getElementById("ttbody");
+
+var dados = [
+  ["Jean Rodrigues", "jeanrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["Lívia Rodrigues", "liviarodrigues@gmail.com", "85-98365-5259", "Bloqueado"],
+  ["Thiago Rodrigues", "thiagorodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["John Rodrigues", "johnrodrigues@gmail.com", "85-98365-5259", "Bloqueado"],
+  ["James Rodrigues", "jamesrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+  ["Jonas Rodrigues", "jonasrodrigues@gmail.com", "85-98365-5259", "Ativo"],
+];
+
+for (var i = 0; i < dados.length; i++) {
+  var tr = "<tr>" +
+    "<td>" + dados [i][0] + "</td>" +
+    "<td>" + dados [i][1] + "</td>" +
+    "<td>" + dados [i][2] + "</td>" +
+    "<td>" + dados [i][3] + "</td>" +
+    "</tr>";
+  ttbody.innerHTML += tr;
+}
+
+document.getElementById("txtBusca").addEventListener("keyup", filtro);
+
+function filtro() {
+
+  var busca = document.getElementById("txtBusca").value.toLowerCase();
+
+  for (var i = 0; i < ttbody.childNodes.length; i++) {
+    var achou = false;
+    var tr = ttbody.childNodes[i];
     var td = tr.childNodes;
 
     for (var j = 0; j < td.length; j++) {
